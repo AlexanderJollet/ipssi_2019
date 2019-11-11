8302f3e2d832f207c20a0f7f777ac93fe191840c
AlexanderJollet <alexander.jollet3@gmail.com>
2019-11-11 11:31:09 +0100

    modif ex03

--- exam2/ex03/calcul.py
+++ exam2/ex03/calcul.py
@@ -2,10 +2,8 @@
 
 def division(a,b):
     if  (b == 0):
-        print ("sorry cannot divide by zero")
-        print ("0")
+        return ("sorry cannot divide by zero\n0")
     else:
         return (a/b)
-
 def multiplication(a,b):
     return (a*b)
