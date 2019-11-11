6ff138983806d2f737087a87e137d44b99e3209a
AlexanderJollet <alexander.jollet3@gmail.com>
2019-11-11 11:47:56 +0100

    ex04 modif des guillemets

--- exam2/ex04/logs.py
+++ exam2/ex04/logs.py
@@ -6,7 +6,7 @@ def logthis(a):
     date = datetime.now()
     datedate = date.strftime("%Y-%m-%d %H:%M:%S")
     print(datedate, a)
-    ecrit = datedate + " " + str(a)
+    ecrit = (datedate+ ' ' +str(a))
     fichier = open("python.log", "a")
     fichier.write(ecrit + "\n")
     fichier.close()
